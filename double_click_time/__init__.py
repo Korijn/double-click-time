7dd76501df7f1e1bfb9001584bfb75c916b93c46
Korijn van Golen <k.vangolen@clinicalgraphics.com>
2022-03-16 14:27:39 +0100

    cleaner

--- double_click_time/__init__.py
+++ double_click_time/__init__.py
@@ -1,13 +1,11 @@
+from ast import Import
 import os
 
 try:
     from win32gui import GetDoubleClickTime as double_click_time
 except ImportError:
-    pass
+    def double_click_time():
+        return int(os.environ.get("DOUBLE_CLICK_TIME", 500))
 
 
 __all__ = ("double_click_time",)
-
-
-def double_click_time():
-    return int(os.environ.get("DOUBLE_CLICK_TIME", 500))
